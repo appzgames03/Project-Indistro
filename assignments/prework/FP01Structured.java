ff1e6d243d2021e0e6777ee6d1d12be2acf76e24
Pavith Sudhir <56886753+PavithSudhirAP@users.noreply.github.com>
2022-09-29 12:42:44 +0530

    Add files via upload

---
+++ assignments/prework/FP01Structured.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class FP01Structured {
+
+	public static void main(String[] args) {
+		
+		printAllNumbersInListStructured(Arrays.asList(1,2));
+		
+	}
+	
+	public static void printAllNumbersInListStructured(List<Integer> numbers) {
+		for(int n:numbers) {
+			System.out.println(n);
+		}
+	}
+	
+	
+}
+
