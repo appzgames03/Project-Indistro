8e61a80614cbbf4685782baecdb81b75b0c3052e
AKASH A MENON <46593739+akashamenon@users.noreply.github.com>
2022-10-25 12:42:12 +0530

    Update EmployeeController.java

--- employeemanagement/controller/EmployeeController.java
+++ employeemanagement/controller/EmployeeController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.greatlearning.employeemanagement.entity.Employee;
 import com.greatlearning.employeemanagement.repository.EmployeeRepository;
 import com.greatlearning.employeemanagement.service.EmployeeService;
-
+//rest controller
 @RestController
 public class EmployeeController {
 System.out.println("Employee Controller");
