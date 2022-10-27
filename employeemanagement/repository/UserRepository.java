b7cb8e15e166797fb153b484b0bd887261170c2c
appzgames03 <88479160+appzgames03@users.noreply.github.com>
2022-10-27 11:59:30 +0530

    Update UserRepository.java

--- employeemanagement/repository/UserRepository.java
+++ employeemanagement/repository/UserRepository.java
@@ -1,5 +1,5 @@
 package com.greatlearning.employeemanagement.repository;
-
+//123456
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
